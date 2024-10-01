6c7045d8983ce9a7b1948350dc02ba2a5acc9b01
alinecintracamargo <alineandradecintra@gmail.com>
2024-10-01 18:54:57 -0300

    feat: consumer function

---
+++ src/ConsumerExample.java
@@ -0,0 +1,28 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        numeros.forEach(n -> {
+                    if (n % 2 == 0) {
+                        System.out.println(n);
+                    }
+                }
+
+
+        );
+
+    }
+}
